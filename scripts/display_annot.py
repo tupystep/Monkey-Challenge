86944e6c39bb973ec3ca03dc350f524a2828a47a
Štěpán Tupý <tupystep@fit.cvut.cz>
2025-03-09 22:06:02 +0100

    Add preprocessing and displaying scripts

---
+++ scripts/display_annot.py
@@ -0,0 +1,155 @@
+import argparse
+import openslide
+import cv2
+import json
+import numpy as np
+from pathlib import Path
+from constants import LYMPHOCYTE_SIZE_UM, MONOCYTE_SIZE_UM
+
+
+def draw_patch(patch_path: Path) -> np.ndarray:
+    """Function loads patch and draws white blood cell labels."""
+    patch = cv2.imread(str(patch_path))
+    with open(patch_path.parents[1] / 'labels' / patch_path.with_suffix('.txt').name) as label_file:
+        lines = label_file.readlines()
+        for line in lines:
+            color = (245, 0, 203) if int(line[0]) == 0 else (0, 0, 229)
+            line = np.array(line.strip().split()[1:]).astype(float)
+
+            center = np.floor((line[:2] + line[2:]/2) * patch.shape[0]).astype(int)
+            cv2.circle(patch, tuple(center), radius=4, color=color, thickness=-1)
+
+            line = np.floor(line * patch.shape[0]).astype(int)
+            cv2.rectangle(patch, tuple(line[:2]), tuple(line[:2] + line[2:]), color=color, thickness=1)
+    return patch
+
+
+def draw_roi(image_path: Path, roi: dict, level: int) -> np.ndarray:
+    """Function loads ROI and draws white blood cell annotations."""
+    slide = openslide.OpenSlide(image_path)
+    image_name = '_'.join(image_path.name.split('_')[:2])
+    annot_path = image_path.parents[2] / 'annotations/json_pixel'
+
+    left_top = np.floor(np.min(roi['polygon'], axis=0)).astype(int)
+    polygon = np.array(roi['polygon']) / slide.level_downsamples[level]
+    left = np.floor(np.min(polygon[:, 0])).astype(int)
+    top = np.floor(np.min(polygon[:, 1])).astype(int)
+    right = np.ceil(np.max(polygon[:, 0])).astype(int)
+    bottom = np.ceil(np.max(polygon[:, 1])).astype(int)
+
+    region = slide.read_region(left_top, level, (right - left + 1, bottom - top + 1))
+    region = cv2.cvtColor(np.array(region), cv2.COLOR_RGBA2BGR)
+    polygon = np.round(polygon - [left, top]).astype(int).reshape((-1, 1, 2))
+    cv2.polylines(region, [polygon], isClosed=True, color=(223, 67, 3), thickness=2)
+
+    lymph_size = LYMPHOCYTE_SIZE_UM / float(slide.properties['openslide.mpp-x']) / slide.level_downsamples[level]
+    lymph_half_size = np.full(2, lymph_size / 2)
+    with open(annot_path / (image_name + '_lymphocytes.json'), 'r') as lymph_file:
+        annot = json.load(lymph_file)
+        for cell in annot['points']:
+            cell = np.array(cell['point']) / slide.level_downsamples[level]
+            if left <= cell[0] < right + 1 and top <= cell[1] < bottom + 1:
+                c1 = (cell - [left, top] - lymph_half_size).round().astype(int)
+                c2 = (cell - [left, top] + lymph_half_size).round().astype(int)
+                cv2.rectangle(region, np.maximum(c1, 0), np.minimum(c2, [right - left, bottom - top]),
+                              color=(245, 0, 203), thickness=1)
+                cell = np.subtract(cell, [left, top]).round().astype(int)
+                cv2.circle(region, tuple(cell), radius=np.maximum(4 - level, 1), color=(245, 0, 203), thickness=-1)
+
+    mono_size =  MONOCYTE_SIZE_UM / float(slide.properties['openslide.mpp-x']) / slide.level_downsamples[level]
+    mono_half_size = np.full(2, mono_size / 2)
+    with open(annot_path / (image_name + '_monocytes.json'), 'r') as mono_file:
+        annot = json.load(mono_file)
+        for cell in annot['points']:
+            cell = np.array(cell['point']) / slide.level_downsamples[level]
+            if left <= cell[0] < right + 1 and top <= cell[1] < bottom + 1:
+                c1 = (cell - [left, top] - mono_half_size).round().astype(int)
+                c2 = (cell - [left, top] + mono_half_size).round().astype(int)
+                cv2.rectangle(region, np.maximum(c1, 0), np.minimum(c2, [right - left, bottom - top]),
+                              color=(0, 0, 229), thickness=1)
+                cell = np.subtract(cell, [left, top]).round().astype(int)
+                cv2.circle(region, tuple(cell), radius=np.maximum(4 - level, 1), color=(0, 0, 229), thickness=-1)
+
+    return region
+
+
+def draw_wsi(image_path: Path, level: int) -> np.ndarray:
+    """Functions loads WSI and draws all ROI borders."""
+    if level < 2:
+        raise RuntimeError('Level must be greater than 1 to fit image into memory')
+
+    slide = openslide.OpenSlide(image_path)
+    image_name = '_'.join(image_path.name.split('_')[:2])
+    annot_path = image_path.parents[2] / 'annotations/json_pixel' / (image_name + '_inflammatory-cells.json')
+
+    level = min(level, slide.level_count - 1)
+    image = slide.read_region((0, 0), level, slide.level_dimensions[level])
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGBA2BGR)
+    with open(annot_path) as f:
+        annot = json.load(f)
+        for roi in annot['rois']:
+            polygon = np.array(roi['polygon']) / slide.level_downsamples[level]
+            polygon = np.round(polygon).astype(int).reshape((-1, 1, 2))
+            cv2.polylines(image, [polygon], isClosed=True, color=(223, 67, 3), thickness=2)
+
+    slide.close()
+    return image
+
+
+def display_wsi(image_path: Path, wsi_level: int, roi_level: int) -> int:
+    """Function gradually displays WSI and its ROIs with annotations."""
+    image_name = '_'.join(image_path.name.split('_')[:2])
+    with open(image_path.parents[2] / 'annotations/json_pixel' / (image_name + '_inflammatory-cells.json')) as f:
+        annot = json.load(f)
+
+    cv2.imshow(image_path.name, draw_wsi(image_path, wsi_level))
+    for roi in annot['rois']:
+        image = draw_roi(image_path, roi, roi_level)
+        key = cv2.waitKey()
+        cv2.destroyAllWindows()
+        if key == ord('q'):
+            return key
+        cv2.imshow(roi['name'], image)
+
+    key = cv2.waitKey()
+    cv2.destroyAllWindows()
+    return key
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script displays random or specified whole slide images or patches. '
+                                                 'If a path to a directory is provided then a series of random images is displayed until terminated. '
+                                                 'Press any key to show next image or press \'q\' to quit at any time. ')
+    parser.add_argument('path', type=Path, help='Path to a directory, whole slide image or a patch')
+    args = parser.parse_args()
+
+    if args.path.is_dir():
+        images = list(args.path.glob('*.tif')) + list(args.path.glob('*.png'))
+        if len(images) == 0:
+            raise RuntimeError(f'No supported images found in {args.path}')
+
+        while True:
+            path = np.random.choice(images)
+            if path.suffix == '.tif':
+                if display_wsi(path, wsi_level=7, roi_level=2) == ord('q'):
+                    break
+            else:
+                cv2.imshow(path.name, draw_patch(path))
+                k = cv2.waitKey()
+                cv2.destroyAllWindows()
+                if k == ord('q'):
+                    break
+
+    elif not args.path.exists():
+        raise RuntimeError(f'File {args.path} does not exist')
+
+    elif args.path.suffix == '.tif':
+        display_wsi(args.path, wsi_level=7, roi_level=2)
+
+    elif args.path.suffix == '.png':
+        cv2.imshow(args.path.name, draw_patch(args.path))
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+    else:
+        raise RuntimeError(f'Invalid file type: {args.path}')
---
+++ scripts/wsi_patching.py
@@ -0,0 +1,115 @@
+import argparse
+import openslide
+import json
+import numpy as np
+from pathlib import Path
+from shapely import Polygon, Point
+from typing import TextIO
+from tqdm import tqdm
+from ultralytics.data.utils import autosplit
+from constants import LYMPHOCYTE_SIZE_UM, MONOCYTE_SIZE_UM
+
+
+def get_coverage(roi: dict, patch_size: int, shift_num: int) -> list[tuple[int, int]]:
+    """Function finds coverage of ROI by patches of same size that are entirely contained within the region."""
+    polygon = Polygon(roi['polygon'])
+    left, top, right, bottom = polygon.bounds
+
+    left = np.floor(left).astype(int)
+    top = np.floor(top).astype(int)
+    right = np.ceil(right).astype(int)
+    bottom = np.ceil(bottom).astype(int)
+
+    best_coverage = []
+    for x_shift in range(0, patch_size, patch_size//shift_num):
+        for y_shift in range(0, patch_size, patch_size//shift_num):
+
+            coverage = []
+            for x in range(left + x_shift, right - patch_size + 2, patch_size):
+                for y in range(top + y_shift, bottom - patch_size + 2, patch_size):
+                    covered = True
+                    for (corner_x, corner_y) in [(x, y), (x + patch_size - 1, y), (x, y + patch_size - 1),
+                                                 (x + patch_size - 1, y + patch_size - 1)]:
+                        if not polygon.contains(Point(corner_x, corner_y)):
+                            covered = False
+                            break
+                    if covered:
+                        coverage.append((x, y))
+            if len(coverage) > len(best_coverage):
+                best_coverage = coverage
+
+    return best_coverage
+
+
+def split_cells(annot: dict, coverage: list[tuple[int, int]], patch_size: int) -> list[list[tuple[float, float]]]:
+    """Function divides cell annotations among created patches."""
+    cells = [[] for _ in range(len(coverage))]
+    for cell in annot['points']:
+        cell = np.array(cell['point'])
+        for i, (x, y) in enumerate(coverage):
+            if x <= cell[0] < x + patch_size and y <= cell[1] < y + patch_size:
+                cells[i].append((cell[0] - x, cell[1] - y))
+                break
+    return cells
+
+
+def write_labels(cells: list[tuple[float, float]], cell_size: float, patch_size: int, label_file: TextIO,
+                 class_label: int) -> None:
+    """Function writes cell annotations for a single patch to label file."""
+    for x, y in cells:
+        x -= cell_size / 2
+        y -= cell_size / 2
+        box_width = (min(x + cell_size, patch_size) - max(x, 0)) / patch_size
+        box_height = (min(y + cell_size, patch_size) - max(y, 0)) / patch_size
+        label_file.write(f'{class_label} {max(x, 0)/patch_size} {max(y, 0)/patch_size} {box_width} {box_height}\n')
+
+
+def patch_image(image_path: Path, patch_size: int, patch_dir: Path, labels_dir: Path, shift_num: int) -> None:
+    """Function splits all ROIs of a WSI into patches and saves them and their annotations."""
+    slide = openslide.OpenSlide(image_path)
+    image_name = '_'.join(image_path.name.split('_')[:2])
+    annot_path = image_path.parents[2] / 'annotations/json_pixel'
+
+    lymph_size = LYMPHOCYTE_SIZE_UM / float(slide.properties['openslide.mpp-x'])
+    mono_size = MONOCYTE_SIZE_UM / float(slide.properties['openslide.mpp-x'])
+
+    with open(annot_path / (image_name + '_lymphocytes.json'), 'r') as lymph_file:
+        lymph_annot = json.load(lymph_file)
+    with open(annot_path / (image_name + '_monocytes.json'), 'r') as mono_file:
+        mono_annot = json.load(mono_file)
+
+    for roi in lymph_annot['rois']:
+        coverage = get_coverage(roi, patch_size, shift_num)
+        lymph_split = split_cells(lymph_annot, coverage, patch_size)
+        mono_split = split_cells(mono_annot, coverage, patch_size)
+        for i, (x, y) in enumerate(coverage):
+            patch = slide.read_region((x, y), 0, (patch_size, patch_size))
+            patch_name = image_path.with_suffix('').name + f'_{roi['name'].replace(' ', '')}_{i}.png'
+            patch.convert('RGB').save(patch_dir / patch_name)
+
+            label_name = Path(patch_name).with_suffix('.txt')
+            with open(labels_dir / label_name, 'x') as label_file:
+                write_labels(lymph_split[i], lymph_size, patch_size, label_file, class_label=0)
+                write_labels(mono_split[i], mono_size, patch_size, label_file, class_label=1)
+
+    slide.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script splits annotated regions of interest into patches of equal size')
+    parser.add_argument('input_directory', type=Path, help='Directory containing whole slide images')
+    parser.add_argument('output_directory', type=Path, help='Output directory where the dataset will be created')
+    parser.add_argument('patch_size', type=int, help='Size of patch side in pixels')
+    args = parser.parse_args()
+
+    input_dir = args.input_directory
+    patch_directory = args.output_directory / (input_dir.name + str(args.patch_size)) / 'images'
+    patch_directory.mkdir(parents=True, exist_ok=False)
+    label_directory = args.output_directory / (input_dir.name + str(args.patch_size)) / 'labels'
+    label_directory.mkdir(parents=True, exist_ok=False)
+
+    image_cnt = len([x for x in input_dir.iterdir()])
+    for wsi_path in tqdm(input_dir.iterdir(), total=image_cnt, desc=f'Patching images in {input_dir}'):
+        patch_image(wsi_path, args.patch_size, patch_directory, label_directory, shift_num=4)
+
+    autosplit(patch_directory, (0.8, 0.1, 0.1))
