0c6b039f9e6aa833bb37520da6e3667d75edbb99
Štěpán Tupý <tupystep@fit.cvut.cz>
2026-02-04 02:21:32 +0100

    Fix comparison utility script

--- scripts/comparison_util.py
+++ scripts/comparison_util.py
@@ -30,9 +30,10 @@ def compare_patches(patches: list[np.ndarray], title: str) -> int:
             rows.append(img)
             img = None
 
-    if img is not None and len(rows) > 0:
-        fill = np.full((img.shape[0], rows[0].shape[1] - img.shape[1], 3), 255, dtype=np.uint8)
-        img = cv2.hconcat([img, fill])
+    if img is not None:
+        if len(rows) > 0:
+            fill = np.full((img.shape[0], rows[0].shape[1] - img.shape[1], 3), 255, dtype=np.uint8)
+            img = cv2.hconcat([img, fill])
         rows.append(img)
 
     img = rows[0]
@@ -66,12 +67,14 @@ def compare_boxes(datasets: list[str], draw_dots: bool = True) -> None:
     for dataset in datasets:
         print(dataset)
 
-    images = list((Path('data') / datasets[0] / 'images').glob('*.png'))
+    _, train_images, _ = get_patch_split(datasets[0])
     while True:
-        patch_name = np.random.choice(images).name
+        patch_name = np.random.choice(train_images).name.replace('IHC', 'PAS')
         patches = []
         for dataset in datasets:
             patch_path = Path('data') / dataset / 'images' / patch_name
+            if 'ihc' in dataset:
+                patch_path = Path('data') / dataset / 'images' / patch_name.replace('PAS', 'IHC')
             patches.append(draw_patch(patch_path, draw_annot=draw_dots))
         key = compare_patches(patches, patch_name)
         if key == ord('q'):
@@ -100,7 +103,7 @@ def compare_nms(dataset: str, model: str, nms: list[float], conf: float) -> None
             break
 
 
-def compare_models(dataset: str, models: list[str], iou: float) -> None:
+def compare_models(dataset: str, models: list[str], nms: float) -> None:
     """Function compares predictions of multiple models."""
     print('Comparing models:')
     print('Patch labels')
@@ -113,7 +116,7 @@ def compare_models(dataset: str, models: list[str], iou: float) -> None:
         patches = [draw_patch(patch_path)]
         for model_name in models:
             model = YOLO(model_name)
-            results = model.predict(patch_path, imgsz=imgsz, conf=0.25, max_det=None, agnostic_nms=True, iou=iou, verbose=False)
+            results = model.predict(patch_path, imgsz=imgsz, conf=0.25, max_det=None, agnostic_nms=True, iou=nms, verbose=False)
             pred_patch = draw_predictions(patch_path, results[0])
             patches.append(pred_patch)
         title = 'TRAIN ' + patch_path.name if patch_path in train_images else 'VAL ' + patch_path.name
@@ -123,9 +126,13 @@ def compare_models(dataset: str, models: list[str], iou: float) -> None:
 
 
 if __name__ == '__main__':
-    # compare_boxes(['basic_box/pas-cpg512', 'seg_box/pas-cpg512', 'pure_seg_box/pas-cpg512'])
+    # compare_boxes(['basic_box/pas-cpg256', 'pure_seg_box/pas-cpg256'])
+    compare_boxes(['basic_box/pas-cpg512', 'pure_seg_box/pas-cpg512', 'pure_seg_box/ihc512'])
+
+    # compare_boxes(['basic_box/pas-cpg256', 'pure_seg_box/pas-cpg256', 'pure_seg_box/pas-cpg256_pad5',
+    #                'pure_seg_box/pas-cpg256_pad10'])
 
     # compare_nms('basic_box/pas-cpg512', 'yolo/basic_box/img512_ep200_yolo11m/weights/last.pt', [0.7, 0.3, 0.1], conf=0.25)
 
-    compare_models('seg_box/pas-cpg256', ['yolo/basic_box/default_aug/img256_ep200_yolo11m/weights/last.pt',
-                    'yolo/basic_box/img256_ep200_yolo11m/weights/last.pt'], iou=0.7)
+    # compare_models('seg_box/pas-cpg256', ['yolo/basic_box/default_aug/img256_ep200_yolo11m/weights/last.pt',
+    #                 'yolo/basic_box/img256_ep200_yolo11m/weights/last.pt'], nms=0.7)
